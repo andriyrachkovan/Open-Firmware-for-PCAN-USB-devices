8c41b0c7c37ad51176ea099c5abcd3d6c3ada2ee
Andriy <31345407+andriyrachkovan@users.noreply.github.com>
2024-02-27 19:52:54 +0100

    Update PCAN_USB.c

--- PCAN_USB.c
+++ PCAN_USB.c
@@ -1,159 +1,176 @@
 #include "PCAN_USB.h"
-static inline void Serial_Init(const uint32_t BaudRate,const bool DoubleSpeed);
-static inline void Serial_SendByte(const char DataByte);
-static inline bool Serial_IsSendReady(void);
-static inline bool Serial_IsSendComplete(void);
-static inline int16_t Serial_ReceiveByte(void);
-void PrintBuffer(const uint8_t* buffer, size_t size);
-static inline bool Serial_IsCharReceived(void);
-void PrintInt(uint16_t int_val);
-void PrintIntDec(uint16_t int_val);
-void UART_TransmitString(const char* String);
 void SetupHardware_(void)
 	{
-	MCUSR &= ~(1 << WDRF);
-	wdt_disable();
-	cli();
-	 CLKPR = (1 << CLKPCE);
-     CLKPR = 0;
-	sei();
-	USB_Init_();
-	TCCR0B = (1 << CS02);
+		cli();
+		wdt_disable();
+		CLKPR = (1 << CLKPCE);
+		CLKPR = 0;
+		sei();
+		
+
+	REGCR  &= ~(1 << REGDIS);
+	UDIEN   = 0;
+	UDINT  = 0;
+	USBCON &= ~(1 << USBE);
+	USBCON |=  (1 << USBE);
+	USBCON &= ~(1 << FRZCLK);
+
+	PLLCSR = ((1 << PLLP0) | (1 << PLLE));
+	while (!(USB_PLL_IsReady_()));
+	
+	
+	Endpoint_ConfigureEndpoint_(0, 0, 16, 1);
+	
+	UDINT  &= ~(1 << SUSPI);
+	UDIEN  |= (1 << SUSPE);
+	UDIEN  |= (1 << EORSTE);
+	UDCON  &= ~(1 << DETACH);
+
+	TCCR0B=(1 << CS02);
+
+//	Serial_Init(1000000,false);
 	AVR__DDR_PORT  |= 0x30;
 	LEDS_OFF;
-//конфигурация UART
-Serial_Init(500000,false);
-
-UART_TransmitString("PCAN-USB(clone)\r\n");
-UART_TransmitString("---------------\r\n");
-PrintIntDec(0xfedc);
-
-UART_TransmitString("\r\n");
-
 	}
 ////////////////////////////////////////////////////
-bool token_77=false;
 int main(void)
 {
-	_delay_ms(10);
     SetupHardware_();
-    RingBuffer_InitBuffer_(&USBtoUSART_Buffer_, USBtoUSART_Buffer_Data_, sizeof(USBtoUSART_Buffer_Data_));
-    RingBuffer_InitBuffer_(&USARTtoUSB_Buffer_, USARTtoUSB_Buffer_Data_, sizeof(USARTtoUSB_Buffer_Data_));
     sei();
 
-
+bool timer_is_acive=false;
+uint32_t timer_EP2=0;	
 for (;;)
-{	
-c0_:
-
-	Endpoint_SelectEndpoint_(1);
+{
+	if(timer_is_acive)
+		if(timer_EP2++>500000)
+		{
+			timer_EP2=0;
+			UENUM=2;
+			UEIENX =1;//заново активируем прерывание IN
+			UECFG0X|=1;//направление IN
+		}
+	UENUM=1;
 	if(Endpoint_IsOUTReceived_())
 	{
 		uint8_t i=0;
 		while(Endpoint_IsReadWriteAllowed_())//читаем пока не выгрузим весь буфер
 		{
-			USBtoUSART_Buffer_Data_[i++]=Endpoint_Read_8_();
+			USBtoUSART_Buffer_Data_[i++]=UEDATX;
 		}
-		Endpoint_ClearOUT_();//подтверждаем его прием
-		UART_TransmitString("CANView DRIVER ");
-		PrintBuffer(USBtoUSART_Buffer_Data_,16);
-		UART_TransmitString(" \r\n");
-		LED1_ALTERNATE;
+		UEINTX &= ~((1 << RXOUTI) | (1 << FIFOCON));
 	}
-	if(memcmp(next_is_bulk_2, USBtoUSART_Buffer_Data_, 4)==0)
+	char t_[]={0x3,0x2,0x1,0x0};//начало балк 2
+	if(memcmp(t_, USBtoUSART_Buffer_Data_, 4)==0)
 	{
-		timer_led1(5000);
-		goto c_1;//знак настройки точки 2 на прием	
+		UENUM=2;
+		if(!timer_is_acive)
+		{
+			UEIENX =1;	//активация прерывания IN
+			UECFG0X|=1;//направление IN
+		}
+		timer_is_acive=true;
 	}
+
 }
-
-
-c_1:	
-	Endpoint_ConfigureEndpoint_(0x2, 2,64, 1);
-	Endpoint_SelectEndpoint_(2);
-	USB_INT_Enable_(USB_INT_RXOUTI_);//включить прерывание OUT
-	USB_INT_Enable_(USB_INT_TXINI_);//включить прерывание IN
-	goto c0_;
-
-	
-
 }
 //////////////////////////////////////////////////////////////////////////
-uint16_t p_cont=0;
 ISR(USB_COM_vect, ISR_BLOCK)
 	{
-	uint8_t PrevSelectedEndpoint = Endpoint_GetCurrentEndpoint_();
+	uint8_t PrevSelectedEndpoint = UENUM;
+    UENUM=2; 
 
-    Endpoint_SelectEndpoint_(2); 
-	if (USB_INT_HasOccurred_(USB_INT_RXOUTI_)&&USB_INT_IsEnabled_(USB_INT_RXOUTI_)) 
+	if ((UEINTX & (1 << TXINI))&&(UEIENX & (1 << TXINE))) 
 	{
-		uint8_t i=0;
-		while(Endpoint_IsReadWriteAllowed_())//читаем пока не выгрузим весь буфер
-		{
-			USBtoUSART_Buffer_Data_[i++]=Endpoint_Read_8_();
-		}
-		Endpoint_ClearOUT_();//подтверждаем его прием
-		uint8_t size_packet=13;
-		if(USBtoUSART_Buffer_Data_[2]==0x28)size_packet=15;
-		UART_TransmitString("CANView ");
-		PrintBuffer(USBtoUSART_Buffer_Data_,size_packet);
-		UART_TransmitString("       ");
-		PrintIntDec(++p_cont);
-		UART_TransmitString(" \r\n");
+		LED2_ALTERNATE;
+		UEDATX=8;
+		UEINTX &= ~((1 << TXINI) | (1 << FIFOCON));
+		while (!(UEINTX &0b10000001));//убедимся что буфер сброшен
+			UEIENX =0b100;	
+			UECFG0X&=0b11111110;//направление OUT
+	}
+
+	if ((UEINTX & (1 << RXOUTI))&&(UEIENX & (1 << RXOUTE))) 
+	{
 		LED1_ALTERNATE;
+		uint8_t i=0;
+		while((UEINTX & (1 << RWAL)))//читаем пока не выгрузим весь буфер
+		{
+			USBtoUSART_Buffer_Data_[i++]=UEDATX;
+		}
+		UEINTX &= ~((1 << RXOUTI) | (1 << FIFOCON));
+		UEINTX &= ~(1 << RXOUTI);
 	}
 	
-	
-	if (USB_INT_HasOccurred_(USB_INT_TXINI_)&&USB_INT_IsEnabled_(USB_INT_TXINI_)) 
-	{
-		UEINTX &= ~(1 << TXINI);
-	}
 
 
-	Endpoint_SelectEndpoint_(ENDPOINT_CONTROLEP_);
-	USB_INT_Disable_(USB_INT_RXSTPI_);
-	GlobalInterruptEnable_();
+	UENUM=0;
+	UEIENX &= ~(1 << RXSTPE);
+	SREG|=0x80;
 	USB_Device_ProcessControlRequest_();
-	Endpoint_SelectEndpoint_(ENDPOINT_CONTROLEP_);
-	USB_INT_Enable_(USB_INT_RXSTPI_);
-	Endpoint_SelectEndpoint_(PrevSelectedEndpoint);
-	return;
+	UENUM=0;
+	UEIENX |= (1 << RXSTPE);
+	UENUM=PrevSelectedEndpoint;
 	}
 
 ISR(USB_GEN_vect, ISR_BLOCK)
 	{
 	
-	if (USB_INT_HasOccurred_(USB_INT_SUSPI_) && USB_INT_IsEnabled_(USB_INT_SUSPI_))
+	if ((UDINT  & (1 << SUSPI)) && (UDIEN  & (1 << SUSPE)))
 		{
-		USB_INT_Disable_(USB_INT_SUSPI_);
-		USB_INT_Enable_(USB_INT_WAKEUPI_);
-		USB_CLK_Freeze_();
-		if (!(USB_Options_ & USB_OPT_MANUAL_PLL_))USB_PLL_Off_();
+		UDIEN  &= ~(1 << SUSPE);
+		UDIEN  |= (1 << WAKEUPE);
+		USBCON |=  (1 << FRZCLK);
+		
+		PLLCSR = 0;
 		USB_DeviceState_ = DEVICE_STATE_Unattached_;
 		}
-	if (USB_INT_HasOccurred_(USB_INT_WAKEUPI_) && USB_INT_IsEnabled_(USB_INT_WAKEUPI_))
+	if ((UDINT  & (1 << WAKEUPI)) && (UDIEN  & (1 << WAKEUPE)))
 		{
 		if (!(USB_Options_ & USB_OPT_MANUAL_PLL_))
 			{
-			USB_PLL_On_();
-			while (!(USB_PLL_IsReady_()));
+				PLLCSR = (1 << PLLP0);
+				PLLCSR = ((1 << PLLP0) | (1 << PLLE));
+				while (!(PLLCSR & (1 << PLOCK)));
 			}
-		USB_CLK_Unfreeze_();
-		USB_INT_Clear_(USB_INT_WAKEUPI_);
-		USB_INT_Disable_(USB_INT_WAKEUPI_);
-		USB_INT_Enable_(USB_INT_SUSPI_);
+		USBCON &= ~(1 << FRZCLK);
+		UDINT  &= ~(1 << WAKEUPI);
+		UDIEN  &= ~(1 << WAKEUPE);
+		UDIEN  |= (1 << SUSPE);
 		}
 
-	if (USB_INT_HasOccurred_(USB_INT_EORSTI_) && USB_INT_IsEnabled_(USB_INT_EORSTI_))
+	if ((UDINT  & (1 << EORSTI)) && (UDIEN  & (1 << EORSTE)))
 		{
-		USB_INT_Clear_(USB_INT_EORSTI_);
-		USB_DeviceState_                = DEVICE_STATE_Default_;
-		USB_INT_Clear_(USB_INT_SUSPI_);
-		USB_INT_Disable_(USB_INT_SUSPI_);
-		USB_INT_Enable_(USB_INT_WAKEUPI_);
-		Endpoint_ConfigureEndpoint_(ENDPOINT_CONTROLEP_, EP_TYPE_CONTROL_,
-		                           USB_Device_ControlEndpointSize_, 1);
-		USB_INT_Enable_(USB_INT_RXSTPI_);
+		UDINT  &= ~(1 << EORSTI);
+		USB_DeviceState_ = 2;
+		UDINT  &= ~(1 << SUSPI);
+		UDIEN  &= ~(1 << SUSPE);
+		UDIEN  |= (1 << WAKEUPE);
+		Endpoint_ConfigureEndpoint_(0, 0,16, 1);
+		UEIENX |= (1 << RXSTPE);
+		}
+	}
+
+ISR(USART1_TX_vect, ISR_BLOCK) 
+{
+		if(ring_buffer[U_ring]!=0x0)
+		{
+			UDR1=ring_buffer[U_ring];
+			U_ring++;
+		}
+}
+
+ISR(USART1_UDRE_vect ISR_BLOCK)
+	{
+		if(timer_LED1++==10000)
+		{
+			timer_LED1=0;		
+			//LED1_ALTERNATE;
+			if(ring_buffer[U_ring]!=0x0)
+			{
+				UDR1=ring_buffer[U_ring];
+				U_ring++;
+			}
 		}
 	}
 
